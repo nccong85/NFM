4e88f7cc2b120e35e75ee190910ffa406fd6d8eb
Nguyen Chi Cong <nccong85@gmail.com>
2018-10-13 02:46:28 +0700

    5rd update

--- src/main/java/vn/com/nsmv/controllers/NFMYCX0010_Controller.java
+++ src/main/java/vn/com/nsmv/controllers/NFMYCX0010_Controller.java
@@ -8,6 +8,7 @@ import vn.com.nsmv.common.BusinessConst;
 
 @Controller
 @RequestMapping(BusinessConst.CodeDef.ScreenId.NFMYCX0010)
+// this is 5rd Update
 public class NFMYCX0010_Controller {
 	// This is 3rd update
 	@RequestMapping(value = "/", method = RequestMethod.GET)
